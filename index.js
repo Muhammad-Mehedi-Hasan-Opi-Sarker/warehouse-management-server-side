47d03a1b0e8de388dbe2980af14f8919bd154508
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-11 16:02:53 +0600

    again set

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
 const express = require('express');
-const cors = require('cors');
-require ('dotenv').config();
-const app =express();
-const port = process.env.PORT ||5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-// middleware 
+let cors =require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors);
-app.use(express.json());
-
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swwzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected')
-  // perform actions on the collection object
-  client.close();
-});
-
-
-
-app.get('/',(req,res)=>{
-    res.send('Server Running')
+app.get('/', (req, res) => {
+  res.send('server running!')
 })
 
-app.listen(port,()=>{
-    console.log(`connected${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
