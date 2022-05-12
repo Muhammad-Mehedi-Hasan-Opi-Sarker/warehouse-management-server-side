c0cf235af609976ec5e1fcec07621887fb856e2b
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-12 12:44:17 +0600

    add update for code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 let cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +8,6 @@ require('dotenv').config()
 // middleware 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swwzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -24,7 +22,13 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     })
-
+    // get for one item 
+    app.get('/product/:id',async(req ,res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    })
   }
   finally {
 
